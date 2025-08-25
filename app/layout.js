f885a202b88dd2d76852fe949d2901eef0524507
Niveditha_M <mniveditha004@gmail.com>
2025-08-26 01:09:21 +0530

    changed srcfolfer

---
+++ app/layout.js
@@ -0,0 +1,27 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Deep-Seek - App",
+  description: "Full Stack Project",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <ClerkProvider>
+    <html lang="en">
+      <body
+        className={`${inter.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+    </ClerkProvider>
+  );
+}
