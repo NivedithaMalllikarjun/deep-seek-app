a16b5a449de8bfb620a04b70a31e899c2f9c5b4e
Niveditha_M <mniveditha004@gmail.com>
2025-08-26 16:07:15 +0530

    modified

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./prism.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { AppContextProvider } from "@/context/AppContext";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -8,20 +11,26 @@ const inter = Inter({
 });
 
 export const metadata = {
-  title: "Deep-Seek - App",
+  title: "DeepSeek - GreatStack",
   description: "Full Stack Project",
 };
 
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={`${inter.className} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <AppContextProvider>
+        <html lang="en">
+          <body className={`${inter.className} antialiased`}>
+            <Toaster toastOptions={
+              {
+                success: {style: { background: "black", color: "white"}},
+                error: {style: { background: "black", color: "white"}}
+              }
+            }/>
+            {children}</body>
+        </html>
+      </AppContextProvider>
     </ClerkProvider>
   );
 }
+
